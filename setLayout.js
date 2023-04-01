bb59b9a17865d9f0770c5ee472ff18d2e0b6349f
dv-jamie <sjm2506@gmail.com>
2023-04-02 03:18:55 +0900

    개인회원 메인 페이지 작업

--- setLayout.js
+++ setLayout.js
@@ -5,9 +5,11 @@ const footer = document.querySelector('#footer');
 
 const pathname = window.location.pathname
 
-fetch('/component/header.html')
-    .then(res => res.text())
-    .then(data => header.innerHTML = data);
+if(pathname === '/main.html') {
+    fetch('/component/header.html')
+        .then(res => res.text())
+        .then(data => header.innerHTML = data);
+}
 
 fetch('/component/gnb.html')
     .then(res => res.text())
@@ -16,13 +18,3 @@ fetch('/component/gnb.html')
 fetch('/component/footer.html')
     .then(res => res.text())
     .then(data => footer.innerHTML = data);
-
-if(pathname.includes('/page/personal')) {
-    fetch('/component/snb-personal.html')
-        .then(res => res.text())
-        .then(data => snb.innerHTML = data);
-} else if(pathname.includes('/page/business')) {
-    fetch('/component/snb-business.html')
-        .then(res => res.text())
-        .then(data => snb.innerHTML = data);
-}
