036c86bbe5cf4ee7607daf4c7183cf0c78d2402e
dv-jamie <sjm2506@gmail.com>
2023-04-04 07:08:03 +0900

    setLayout.js 수정

--- setLayout.js
+++ setLayout.js
@@ -1,22 +1,20 @@
-window.addEventListener('load', function() {
-    const header = document.querySelector('#header');
-    const gnb = document.querySelector('#gnb');
-    const snb = document.querySelector('#snb');
-    const footer = document.querySelector('#footer');
-    
-    const pathname = window.location.pathname
-    
-    if(pathname === '/main.html') {
-        fetch('./component/header.html')
-            .then(res => res.text())
-            .then(data => header.innerHTML = data);
-    }
-    
-    fetch('./component/gnb.html')
+const header = document.querySelector('#header');
+const gnb = document.querySelector('#gnb');
+const snb = document.querySelector('#snb');
+const footer = document.querySelector('#footer');
+
+const pathname = window.location.pathname
+
+if(pathname === '/main.html') {
+    fetch('./component/header.html')
         .then(res => res.text())
-        .then(data => gnb.innerHTML = data);
-    
-    fetch('./component/footer.html')
-        .then(res => res.text())
-        .then(data => footer.innerHTML = data);
-})
+        .then(data => header.innerHTML = data);
+}
+
+fetch('./component/gnb.html')
+    .then(res => res.text())
+    .then(data => gnb.innerHTML = data);
+
+fetch('./component/footer.html')
+    .then(res => res.text())
+    .then(data => footer.innerHTML = data);
